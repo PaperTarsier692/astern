129c427a9bacf3c3dd175da68c0b97262cabc001
PaperTarsier692 <moritz.harrer@icloud.com>
2025-12-10 12:25:53 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,161 @@
+#include "raylib.h"
+#include "raymath.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <float.h>
+
+#define X 1440
+#define Y 750
+#define FPS 60
+#define SPEED 1
+#define POINTS 500
+#define RADIUS 5
+#define CONNECTION_DIST 50000
+#define MAX_CONNECTIONS 10
+
+typedef struct Point Point;
+
+typedef struct Entry
+{
+    Point *point;
+    float distance;
+} Entry;
+
+typedef struct Point
+{
+    Vector2 pos;
+    Vector2 vel;
+    Entry connected[MAX_CONNECTIONS];
+} Point;
+
+typedef struct
+{
+    Vector2 points[POINTS];
+    unsigned int pointCount;
+    size_t length;
+} Path;
+
+Point points[POINTS];
+unsigned int ballCounter;
+
+Vector2 getRandomPos()
+{
+    return (Vector2){GetRandomValue(0, X), GetRandomValue(0, Y)};
+}
+
+Vector2 getRandomVel()
+{
+    return (Vector2){(float)GetRandomValue(-SPEED, SPEED), (float)GetRandomValue(-SPEED, SPEED)};
+}
+
+void initPoints()
+{
+    for (unsigned int i = 0; i < POINTS; i++)
+        points[i] = (Point){
+            getRandomPos(),
+            getRandomVel()};
+}
+
+void movePoint(Point *point)
+{
+    point->pos = Vector2Add(point->pos, point->vel);
+    if (point->pos.x > X)
+        point->pos.x -= X;
+    if (point->pos.y > Y)
+        point->pos.y -= Y;
+    if (point->pos.x < 0)
+        point->pos.x += X;
+    if (point->pos.y < 0)
+        point->pos.y += Y;
+}
+
+void movePoints()
+{
+    for (unsigned int i = 0; i < POINTS; i++)
+        movePoint(&points[i]);
+}
+
+void drawPoint(Point point)
+{
+    DrawCircleV(point.pos, RADIUS, (Color){200, 200, 200, 100});
+}
+
+void getNearestPoints(Point *point, int index)
+{
+    Point *other;
+    float distance;
+    Entry *entries = point->connected;
+    for (int j = 0; j < MAX_CONNECTIONS; j++)
+        entries[j].distance = FLT_MAX;
+    for (int i = 0; i < POINTS; i++)
+    {
+        if (i == index)
+            continue;
+        other = &points[i];
+        distance = Vector2DistanceSqr(point->pos, other->pos);
+        if (distance > CONNECTION_DIST)
+            continue;
+        int maxIdx = 0;
+        float maxDist = entries[0].distance;
+        for (int j = 1; j < MAX_CONNECTIONS; j++)
+        {
+            if (entries[j].distance > maxDist)
+            {
+                maxDist = entries[j].distance;
+                maxIdx = j;
+            }
+        }
+        if (distance < maxDist)
+            entries[maxIdx] = (Entry){other, distance};
+    }
+}
+
+void drawPoints()
+{
+    for (unsigned int i = 0; i < POINTS; i++)
+        drawPoint(points[i]);
+}
+
+void drawConnection(Point point, int index)
+{
+    getNearestPoints(&point, index);
+    for (int i = 0; i < MAX_CONNECTIONS; i++)
+        if (point.connected[i].distance < FLT_MAX)
+            DrawLineV(point.pos, point.connected[i].point->pos, (Color){100, 255, 100, 255 - Clamp(point.connected[i].distance / 100, 20, 255)});
+}
+
+void getPath(Point start, Point end)
+{
+}
+
+void drawConnections()
+{
+    for (unsigned int i = 0; i < POINTS; i++)
+        drawConnection(points[i], i);
+}
+
+int main(void)
+{
+    srand(time(NULL));
+    SetTraceLogLevel(LOG_WARNING);
+    InitWindow(X, Y, "Kein Stern, nur A - Raylib");
+    SetTargetFPS(FPS);
+    SetRandomSeed(time(NULL));
+    initPoints();
+
+    while (!WindowShouldClose())
+    {
+        if (IsKeyPressed(KEY_F))
+            ToggleFullscreen();
+        BeginDrawing();
+        ClearBackground(GRAY);
+        movePoints();
+        drawConnections();
+        drawPoints();
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+    CloseWindow();
+    return EXIT_SUCCESS;
+}
